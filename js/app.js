569b02a9366f55d7d15b099ea9f3b750825e9894
Robert Pearce <robertwaltonpearce@gmail.com>
2015-03-25 23:54:41 -0600

    send email forms

--- js/app.js
+++ js/app.js
@@ -1,42 +1,82 @@
 ;(function() {
-  var request, method, url, data, process;
-  request = new XMLHttpRequest();
-  method = 'GET';
-  url = '';
-  data = JSON.stringify({
-    'key': 'Y40QePckhKKYTmtJB8I8Wg',
-    'message': {
-      'from_email': 'me@robertwpearce.com',
-      'to': [
-        {
-          'email': 'robertwaltonpearce@gmail.com',
-          'name': 'Robert Pearce',
-          'type': 'to'
+  var _sendEmail = function(data, form) {
+    var request, method, url, async, process;
+    request = new XMLHttpRequest();
+    method = 'POST';
+    url = 'http://production-mhtciurjts.elasticbeanstalk.com/send_email';
+    async = true;
+    process = function(request) {
+      var inputs, button, i;
+      inputs = form.querySelectorAll('input');
+      button = form.querySelector('button');
+      i = 0;
+      for(; i < inputs.length; i+=1) {
+        inputs[i].value = '';
+      }
+      button.innerText = 'Submitted!';
+      setTimeout(function() {
+        button.innerText = 'Submit';
+      }, 3000);
+    };
+
+    if (request.withCredentials != null) {
+      request.open(method, url, async);
+      request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+      request.onreadystatechange = function() {
+        if (request.readyState === 4 && request.status === 200) {
+          return process(request);
         }
-      ],
-      'autotext': 'true',
-      'subject': 'TESTY',
-      'html': 'This is a test bro'
+      };
+    } else if (typeof XDomainRequest !== "undefined" && XDomainRequest != null) {
+      request = new XDomainRequest();
+      request.open(method, url);
+      request.onload = function() {
+        return process(request);
+      };
     }
-  });
-  process = function(request) {
-    console.log(request);
+    request.send(JSON.stringify(data));
   };
 
-  if (request.withCredentials != null) {
-    request.open(method, url, true);
-    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-    request.send(data);
-    request.onreadystatechange = function() {
-      if (request.readyState === 4) {
-        return process(request);
-      }
+  var _getSignupValues = function(e) {
+    var form, button, data;
+    e.preventDefault();
+    form = e.target;
+    button = form.querySelector('button');
+    data = {
+      type: 'Normal Signup',
+      name: form.querySelector('#signup-name').value,
+      email: form.querySelector('#signup-email').value
     };
-  } else if (typeof XDomainRequest !== "undefined" && XDomainRequest != null) {
-    request = new XDomainRequest();
-    request.open(method, url);
-    request.onload = function() {
-      return process(request);
+    button.innerText = 'Sending...'
+    if (data.name != "" && data.email != null) {
+      _sendEmail(data, form);
+    } else {
+      alert('Please provide your name and email address');
+    }
+  };
+
+  var _getShopSignupValues = function(e) {
+    var form, button, data;
+    e.preventDefault();
+    form = e.target;
+    button = form.querySelector('button');
+    button.innerText = 'Sending...'
+    data = {
+      type: 'Participating Shop',
+      name: form.querySelector('#participate-name').value,
+      email: form.querySelector('#participate-email').value,
+      telephone: form.querySelector('#participate-telephone').value,
+      shop: form.querySelector('#participate-shop').value
     };
-  }
+    if (data.name !== "" && data.email !== "" && data.telephone !== "" && data.shop !== "") {
+      _sendEmail(data, form);
+    } else {
+      alert('Please fill in all the fields');
+    }
+  };
+
+  var signupForm = document.getElementById('form-signup');
+  var shopSignupForm = document.getElementById('form-participate');
+  signupForm.addEventListener('submit', _getSignupValues, false);
+  shopSignupForm.addEventListener('submit', _getShopSignupValues, false);
 })();
